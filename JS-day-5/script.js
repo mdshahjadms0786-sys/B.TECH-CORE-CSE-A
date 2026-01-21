7e2c8bcab3bdccc276b81c4fde396f626c3730b2
Md Shahjad <mdshahjadms0786-sys@gmail.com>
2026-01-21 18:57:30 +0530

    Initial commit - all class work added

---
+++ JS-day-5/script.js
@@ -0,0 +1,29 @@
+// let str="JavaScript id fun";
+// let sliced=str.slice(0,10);
+// console.log(sliced)
+
+
+// let str = "JavaScript is amazing!";
+// let sub = str.substring(11, 17);
+
+// console.log(sub)
+
+// const paragraph = 'The quick brown fox jumps over the lazy dog. If the dog barked, was it really lazy?';
+// const indexOfFirst = paragraph.indexOf("dog");
+// console.log(indexOfFirst);
+
+// let firstName = "John";
+// let lastName = "Doe";
+// let fullName = firstName.concat(" ", lastName); 
+// console.log(fullName);
+
+
+//let sentence = "I,am,a,comma,separated,sentence";
+// let words = sentence.split(",");
+// console.log(words)
+// console.log()
+
+
+let str = "tu meri me tera me tera tu meri"
+let sp=str.split("r")
+console.log(sp)
